3310ac4a068d5c690d86a0e895fdb4d824c5600a
Matthew Sweda <msweda.developer@gmail.com>
2019-11-28 10:39:42 -0700

    Remove eslint parser option

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports =  {
     'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  parser: "babel-eslint",
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
